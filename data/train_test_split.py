af0ef206c7377ab544e09a184fbb4e59105f1695
Leon <s6leherb@uni-bonn.de>
2024-01-21 23:37:23 +0100

    Recreated test and validation set.

--- data/train_test_split.py
+++ data/train_test_split.py
@@ -10,13 +10,13 @@ def main():
     # Rename 'raw_response' to 'response'.
     data = data.rename(columns={"raw_response": "response"})
     data = data.sample(frac=1, random_state=42)
-    train, val, test = (
+    train, test, val = (
         data[: int(len(data) * 0.7)],
         data[int(len(data) * 0.7) : int(len(data) * 0.9)],
         data[int(len(data) * 0.9) :],
     )
     train.to_csv("train.csv", index=False)
-    val.to_csv("val.csv", index=False)
+    val.to_csv("validation.csv", index=False)
     test.to_csv("test.csv", index=False)
 
 
