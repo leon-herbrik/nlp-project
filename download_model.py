956e488a262f9d7ead491e03030d57764a9f4000
Leon <s6leherb@uni-bonn.de>
2024-01-21 21:54:19 +0100

    Reinstalled llama_recipes.

--- download_model.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
