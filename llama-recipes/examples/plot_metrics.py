79c64f083fbe4d7cf7385995d62b8f7c8fcce826
Leon <s6leherb@uni-bonn.de>
2024-01-23 21:43:43 +0100

    Update variable names in plot_metric function

--- llama-recipes/examples/plot_metrics.py
+++ llama-recipes/examples/plot_metrics.py
@@ -6,8 +6,8 @@ import os
 def plot_metric(data, metric_name, x_label, y_label, title, colors):
     plt.figure(figsize=(7, 6))
     
-    plt.plot(data[f'train_epoch_{metric_name}'], label=f'Train Epoch {metric_name.capitalize()}', color=colors[0])
-    plt.plot(data[f'val_epoch_{metric_name}'], label=f'Validation Epoch {metric_name.capitalize()}', color=colors[1])
+    plt.plot(data[f'train_{metric_name}'], label=f'Train Epoch {metric_name.capitalize()}', color=colors[0])
+    plt.plot(data[f'val_{metric_name}'], label=f'Validation Epoch {metric_name.capitalize()}', color=colors[1])
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(f'Train and Validation Epoch {title}')
