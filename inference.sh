318565b2a29fa461cd98c2f8ce797259259063d4
Leon <s6leherb@uni-bonn.de>
2024-01-24 13:41:21 +0100

    Update inference.sh script to use correct path for peft_model

--- inference.sh
+++ inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=A100medium
+#SBATCH --partition=A40medium
 #SBATCH --time=23:59:59
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 cd /home/s6leherb/nlp-project/llama-recipes
-python examples/inference.py --model_name /home/s6leherb/nlp-project/model/Llama-2-7b-chat-hf --peft_model /home/s6leherb/nlp-project/model/finetunes/epoch_0 --prompt_file /home/s6leherb/nlp-project/prompt.txt
+python examples/inference.py --model_name /home/s6leherb/nlp-project/model/Llama-2-7b-chat-hf --peft_model /home/s6leherb/nlp-project/model/finetunes/epochs/epoch_0 --prompt_file /home/s6leherb/nlp-project/prompt.txt
