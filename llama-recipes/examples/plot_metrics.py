dbb38df49d8ac4cd894e84ef1eea4a052e7da1d2
wndlbltt <leon@herbrik.de>
2024-02-01 08:32:33 +0100

    Update plot_metrics function to include a maximum epoch parameter

--- llama-recipes/examples/plot_metrics.py
+++ llama-recipes/examples/plot_metrics.py
@@ -5,38 +5,73 @@ import os
 import seaborn as sns
 
 
-def plot_metric(data, metric_name, x_label, y_label, title, colors, max_epoch=10):
-    plt.figure(figsize=(7, 6))
+def plot_metric(data, metric_name, x_label, y_label, title, colors, max_epoch=30):
+    # plt.figure(figsize=(7, 6))
+    # Create single plot.
+    fig, ax = plt.subplots(figsize=(413 / 40, 238 / 40))
+    # Create twin y axis.
+    ax2 = ax.twinx()
 
-    plt.plot(
+    dim = range(1, max_epoch + 1)
+
+    # Enable grid.
+    ax.grid(True)
+
+    train = ax.plot(
+        dim,
         data[f"train_{metric_name}"][:max_epoch],
-        label=f"Train Epoch {metric_name.capitalize()}",
+        label=f"Train {metric_name.capitalize()}",
         color=colors[0],
+        lw=3,
     )
-    plt.plot(
+    val = ax.plot(
+        dim,
         data[f"val_{metric_name}"][:max_epoch],
-        label=f"Validation Epoch {metric_name.capitalize()}",
+        label=f"Validation {metric_name.capitalize()}",
         color=colors[1],
+        lw=3,
     )
-    plt.plot(
+    alpaca = ax.plot(
+        dim,
         data[f"alpaca_{metric_name}"][:max_epoch],
-        label=f"Alpaca Validation Epoch {metric_name.capitalize()}",
+        label=f"Validation {metric_name.capitalize()} 'Alpaca'",
         color=colors[2],
+        lw=3,
     )
-    plt.plot(
-        data[f"rhyming"][:max_epoch],
+    # Smooth rhyming percentage.
+    rhyming_data = data[f"rhyming"][:max_epoch]
+    rhyming_data_smooth = []
+    for i in range(len(rhyming_data)):
+        if i < 10:
+            rhyming_data_smooth.append(rhyming_data[i])
+        else:
+            rhyming_data_smooth.append(sum(rhyming_data[i - 5 : i]) / 5)
+
+    rhyming = ax2.plot(
+        dim,
+        rhyming_data_smooth,
         label=f"Rhyming Percentage",
         color=colors[3],
+        linestyle="--",
+        lw=3,
     )
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(f"Train and Validation Epoch {title}")
-    plt.legend()
-    plt.tight_layout()
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax2.set_ylabel("Rhyming Percentage")
+    ax.set_title(f"Validation Metrics")
+    # Collect all plots for common legend.
+    plots = train + val + alpaca + rhyming
+    # Collect all labels for common legend.
+    labels = [l.get_label() for l in plots]
+    # Create common legend.
+    ax.legend(plots, labels, loc=0)
+    fig.tight_layout()
 
 
-def plot_single_metric_by_step(data, metric_name, x_label, y_label, title, color):
-    plt.plot(data[f"{metric_name}"], label=f"{title}", color=color)
+def plot_single_metric_by_step(
+    data, metric_name, x_label, y_label, title, color, max_epoch
+):
+    plt.plot(data[f"{metric_name}"][:max_epoch], label=f"{title}", color=color)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
@@ -44,7 +79,7 @@ def plot_single_metric_by_step(data, metric_name, x_label, y_label, title, color
     plt.tight_layout()
 
 
-def plot_metrics_by_step(data, metric_name, x_label, y_label, colors):
+def plot_metrics_by_step(data, metric_name, x_label, y_label, colors, max_epoch):
     plt.figure(figsize=(14, 6))
 
     plt.subplot(1, 3, 1)
@@ -55,6 +90,7 @@ def plot_metrics_by_step(data, metric_name, x_label, y_label, colors):
         y_label,
         f"Train Step {metric_name.capitalize()}",
         colors[0],
+        max_epoch,
     )
     plt.subplot(1, 3, 2)
     plot_single_metric_by_step(
@@ -64,6 +100,7 @@ def plot_metrics_by_step(data, metric_name, x_label, y_label, colors):
         y_label,
         f"Validation Step {metric_name.capitalize()}",
         colors[1],
+        max_epoch,
     )
     plt.subplot(1, 3, 3)
     plot_single_metric_by_step(
@@ -73,11 +110,12 @@ def plot_metrics_by_step(data, metric_name, x_label, y_label, colors):
         y_label,
         f"Alpaca Validation Step {metric_name.capitalize()}",
         colors[2],
+        max_epoch,
     )
     plt.tight_layout()
 
 
-def plot_metrics(file_path):
+def plot_metrics(file_path, max_epoch=10):
     if not os.path.exists(file_path):
         print(f"File {file_path} does not exist.")
         return
@@ -92,35 +130,47 @@ def plot_metrics(file_path):
     directory = os.path.dirname(file_path)
     filename_prefix = os.path.basename(file_path).split(".")[0]
     number_of_metrics = 4
-    colors = sns.color_palette("crest", number_of_metrics)
+    # rojo = "#DD1C1A"
+    # plum = 9C528B
+    colors = ["#588b8b", "#ab9e93", "#9C528B", "#1E1014"]
 
-    plot_metric(data, "loss", "Epoch", "Loss", "Loss", colors)
+    plot_metric(data, "loss", "Epoch", "Loss", "Loss", colors, max_epoch)
     plt.savefig(
-        os.path.join(directory, f"{filename_prefix}_train_and_validation_loss.png")
+        os.path.join(directory, f"{filename_prefix}_train_and_validation_loss.png"),
+        format="png",
+        transparent=True,
     )
     plt.close()
 
-    plot_metric(data, "perplexity", "Epoch", "Perplexity", "Perplexity", colors)
+    plot_metric(
+        data, "perplexity", "Epoch", "Perplexity", "Perplexity", colors, max_epoch
+    )
     plt.savefig(
         os.path.join(
             directory, f"{filename_prefix}_train_and_validation_perplexity.png"
-        )
+        ),
+        format="png",
+        transparent=True,
     )
     plt.close()
 
-    plot_metrics_by_step(data, "loss", "Step", "Loss", colors)
+    plot_metrics_by_step(data, "loss", "Step", "Loss", colors, max_epoch)
     plt.savefig(
         os.path.join(
             directory, f"{filename_prefix}_train_and_validation_loss_by_step.png"
-        )
+        ),
+        format="png",
+        transparent=True,
     )
     plt.close()
 
-    plot_metrics_by_step(data, "perplexity", "Step", "Perplexity", colors)
+    plot_metrics_by_step(data, "perplexity", "Step", "Perplexity", colors, max_epoch)
     plt.savefig(
         os.path.join(
             directory, f"{filename_prefix}_train_and_validation_perplexity_by_step.png"
-        )
+        ),
+        format="png",
+        transparent=True,
     )
     plt.close()
 
@@ -130,6 +180,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--file_path", required=True, type=str, help="Path to the metrics JSON file."
     )
+    # Number of epochs to plot.
+    parser.add_argument(
+        "--max_epoch",
+        required=False,
+        type=int,
+        default=30,
+        help="Number of epochs to plot.",
+    )
     args = parser.parse_args()
-
-    plot_metrics(args.file_path)
+    # Update plt font size.
+    plt.rcParams.update({"font.size": 13})
+    plot_metrics(args.file_path, args.max_epoch)
