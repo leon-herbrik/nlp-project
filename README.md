8f2d1f32a8acd219b1e26f8d7db196c2207dbdf1
Leon <s6leherb@uni-bonn.de>
2024-02-10 19:28:20 +0100

    Update LLama 2 pipeline description

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Fine-tune LLama 2 on Synthetically Generated Data
 In this project, we use a three-step pipeline to generate training data for LLama 2 and fine-tune it on the generated data. The pipeline consists of:
 1. Prompting LLama 2 to generate questions like 'Does the moon have its own light?'.
-2. Querying LLama to answer the questions adhering to a certain role (in our case, 'poet'): 'The moon, a glowing orb in the night, its light, a beacon, a gentle delight'.
-3. Fine-tuning LLama 2 on the generated data.
+2. Querying the model to answer the questions adhering to a certain role (in our case, 'poet'): 'The moon, a glowing orb in the night, its light, a beacon, a gentle delight'.
+3. Fine-tuning on the generated data.
 
 ## Install
 Create a new conda environment from the nlp.yml file:
 ```bash
 conda env create -f nlp.yml
 ```
+
+## Usage
+For step 1 of the pipeline
---
+++ generate_questions.py
@@ -0,0 +1,45 @@
+"""
+This script is for step 1 of the pipeline. It is used to generate a predefined number of general purpose questions from the model.
+"""
+
+# Standard packages
+import os
+import sys
+import json
+import argparse
+
+# Local packages
+from inference import inference
+
+
+def main():
+    """
+    Generate n general purpose questions.
+    Store them in a JSON file.
+
+    @param n: Number of questions to generate.
+    @param output_file: Path to the output file. 
+    """
+    parser = argparse.ArgumentParser(description="Generate general purpose questions.")
+    parser.add_argument(
+        "-n", "--number_of_questions", required=False, default=750, type=int, help="Number of questions to generate."
+    )
+    parser.add_argument(
+        "-o", "--output_file", required=False, default="questions.json", type=str, help="Path to the output file."
+    )
+    categories = ["wh-questions", "yes/no questions", "explain", "create"]
+    prompts = ["Create 50 wh-questions about general topics. Example: What is the capital of France? Cover as many topics and types of questions as possible. Don't repeat questions.",
+                "Create 50 yes/no questions about general topics. Example: Is the sun a star? Cover as many topics and types of questions as possible. Don't repeat questions.",
+                "Explain 50 general topics. Example: Explain the concept of entropy. Cover as many topics and types of questions as possible. Don't repeat questions.",
+                "Create 50 general topics. Example: Create a poem about the universe. Cover as many topics and types of questions as possible. Don't repeat questions."]
+    args = parser.parse_args()
+    prompt = ""
+    prompt_template = f'''[INST] <<SYS>>
+    You are a helpful, respectful and honest assistant. However it is your role to only answer in poems or rhymes. Use a pair-rhyme for answering.
+    <</SYS>>
+    {prompt}[/INST]'''
+    pass
+
+
+if __name__ == "__main__":
+    main()
